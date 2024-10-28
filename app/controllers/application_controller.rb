3e4b02e2bed4dbce35165ccfd284a456f89d0312
Tclewis13 <the3rdi@gmail.com>
2024-10-27 23:43:00 -0400

    project done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 require "application_responder"
 
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
   self.responder = ApplicationResponder
   respond_to :html
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
   def new
     @post = Post.new
   end
